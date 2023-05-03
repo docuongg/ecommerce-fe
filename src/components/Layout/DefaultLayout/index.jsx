866a5277ff897fe4357c4ed39a1d00468dc7c1f1
docuongg <dncuong168@gmail.com>
2023-05-03 10:17:34 +0700

    add-chat-messenger-plugin

--- src/components/Layout/DefaultLayout/index.jsx
+++ src/components/Layout/DefaultLayout/index.jsx
@@ -1,6 +1,7 @@
 import { Footer, Announcement, Newsletter } from '../components/user'
 import { Header } from '../components'
 import styled from "styled-components"
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
 
 const ChildContainer = styled.div`
   background-color: #FFFFFF;
